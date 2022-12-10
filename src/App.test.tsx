25d0a479a06ea63b310362899d9bb7544fb9ab8e
jmRakoto <jannymichelr@gmail.com>
2022-12-10 22:29:38 +0300

    test must failed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders Kaiza text", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Kaiza/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App />", () => {
+  test("renders Kaiza text", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Kaiza/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Kaiza</p>
+        <p>Miova ary</p>
+        <p>Salut</p>
       </header>
       <Counter />
     </div>
